0b18ce170bab33e22ea8d64c83dee95868062822
zhangtusheng <908647593@qq.com>
2023-10-18 20:15:17 +0800

    dp

--- algo/src/main/java/com/zts/dp/DpMain.java
+++ algo/src/main/java/com/zts/dp/DpMain.java
@@ -24,6 +24,28 @@ public class DpMain {
 
 
 	/**
+	 * https://leetcode.cn/leetbook/read/illustration-of-algorithm/ozzl1r/
+	 * @param num
+	 * @return
+	 */
+	public double[] statisticsProbability(int num) {
+		double[] dp = new double[6];
+		Arrays.fill(dp, 1.0 / 6.0);
+		for (int i = 2; i <= num; i++) {
+			double[] tmp = new double[5 * i + 1];
+			for (int j = 0; j < dp.length; j++) {
+				for (int k = 0; k < 6; k++) {
+					tmp[j + k] += dp[j] / 6.0;
+				}
+			}
+			dp = tmp;
+		}
+		return dp;
+	}
+
+
+
+	/**
 	 * https://leetcode.cn/leetbook/read/illustration-of-algorithm/9h3im5/
 	 * @param n
 	 * @return
