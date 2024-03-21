8d23c3232afc273f357907b3367eeaf610e80fbc
zhangtusheng <908647593@qq.com>
2024-03-21 22:48:09 +0800

    字符串dp

--- algo/src/main/java/com/zts/dp/DpMain2024.java
+++ algo/src/main/java/com/zts/dp/DpMain2024.java
@@ -61,8 +61,33 @@ public class DpMain2024 {
 //		System.out.println(dpMain2024.maxEnvelopes(new int[][] {{1,1}, {1,1}, {1,1}}));
 //		System.out.println(dpMain2024.findLongestChain(new int[][]{{1,2}, {2,3}, {3,4}}));
 
-		System.out.println(dpMain2024.longestCommonSubsequence("abcde", "ace"));
+//		System.out.println(dpMain2024.longestCommonSubsequence("abcde", "ace"));
+		System.out.println(dpMain2024.maxUncrossedLines(new int[]{2,5,1,2,5}, new int[]{10,5,2,1,5,2}));
+		System.out.println(dpMain2024.maxUncrossedLines(new int[]{1,3,7,1,7,5}, new int[]{1,9,2,5,1}));
 
+	}
+
+
+	/**
+	 * https://leetcode.cn/problems/uncrossed-lines/?envType=study-plan-v2&envId=dynamic-programming
+	 * @param nums1
+	 * @param nums2
+	 * @return
+	 */
+	public int maxUncrossedLines(int[] nums1, int[] nums2) {
+		int m = nums1.length;
+		int n = nums2.length;
+		int[][] dp = new int[m+1][n+1];
+		for (int i = 1; i <=m ; i++) {
+			for (int j = 1; j <=n ; j++) {
+				if (nums1[i-1] == nums2[j-1]) {
+					dp[i][j] = dp[i-1][j-1] + 1;
+				} else {
+					dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+				}
+			}
+		}
+		return dp[m][n];
 	}
 
 
