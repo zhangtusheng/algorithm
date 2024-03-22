45d1d9d30fe4c134c7436492379b6dd7977276c5
zhangtusheng <908647593@qq.com>
2024-03-22 23:10:22 +0800

    二维dp

--- algo/src/main/java/com/zts/dp/DpMain2024.java
+++ algo/src/main/java/com/zts/dp/DpMain2024.java
@@ -98,6 +98,26 @@ public class DpMain2024 {
 
 	}
 
+
+	/**
+	 * https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-iii/?envType=study-plan-v2&envId=dynamic-programming
+	 * @param prices
+	 * @return
+	 */
+	public int maxProfit2(int[] prices) {
+		int n = prices.length;
+		int buy1 = -prices[0], sell1 = 0;
+		int buy2 = -prices[0], sell2 = 0;
+		for (int i = 1; i < n; ++i) {
+			buy1 = Math.max(buy1, -prices[i]);
+			sell1 = Math.max(sell1, buy1 + prices[i]);
+			buy2 = Math.max(buy2, sell1 - prices[i]);
+			sell2 = Math.max(sell2, buy2 + prices[i]);
+		}
+		return sell2;
+
+	}
+
 	/**
 	 * https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/?envType=study-plan-v2&envId=dynamic-programming
 	 * @param prices
